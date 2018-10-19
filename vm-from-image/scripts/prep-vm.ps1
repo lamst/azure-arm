5c046d97fb45b0b2452f0b7a5a888e47b7b5f5df
Stanley Lam <lam.stanley@gmail.com>
2018-10-19 19:04:11 +0800

    Fix extension custom script

--- vm-from-image/scripts/prep-vm.ps1
+++ vm-from-image/scripts/prep-vm.ps1
@@ -1,6 +1,6 @@
 # Install modules to update Windows
-Install-PackageProvider -Name "Nuget" -Force:$true -Confirm:$false
-Install-Module PSWindowsUpdate -Confirm:$false -Force:$true -SkipPublisherCheck:$true
+# Install-PackageProvider -Name "Nuget" -Force:$true -Confirm:$false
+# Install-Module PSWindowsUpdate -Confirm:$false -Force:$true -SkipPublisherCheck:$true
 Get-WUInstall –MicrosoftUpdate –AcceptAll -IgnoreReboot
 
 # Generalize the VM
