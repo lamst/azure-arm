39ea0adeba190676a4d66b985b19c474d9e0c591
Stanley Lam <lam.stanley@gmail.com>
2018-10-22 15:13:28 +0800

    Added commands to install PS updater

--- vm-from-image/scripts/prep-vm.ps1
+++ vm-from-image/scripts/prep-vm.ps1
@@ -1,8 +1,8 @@
 # Install modules to update Windows
-# Install-PackageProvider -Name "Nuget" -Force:$true -Confirm:$false
-# Install-Module PSWindowsUpdate -Confirm:$false -Force:$true -SkipPublisherCheck:$true
+Install-PackageProvider -Name "Nuget" -Force:$true -Confirm:$false
+Install-Module PSWindowsUpdate -Confirm:$false -Force:$true -SkipPublisherCheck:$true -Scope AllUsers
 Get-WUInstall –MicrosoftUpdate –AcceptAll -IgnoreReboot -Confirm:$false
 
 # Generalize the VM
-$sysprepPath = $env:windir + '\System32\Sysprep\sysprep.exe'
-Start-Process -FilePath $sysprepPath -ArgumentList ("/oobe /shutdown /generalize /quiet")
+# $sysprepPath = $env:windir + '\System32\Sysprep\sysprep.exe'
+# Start-Process -FilePath $sysprepPath -ArgumentList ("/oobe /shutdown /generalize /quiet")
