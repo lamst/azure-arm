73ad140f2aed6f83db55e6a38f3ab0a5f37367fa
Stanley Lam <lam.stanley@gmail.com>
2018-10-22 15:40:06 +0800

    Fix incorrect characters

--- vm-from-image/scripts/prep-vm.ps1
+++ vm-from-image/scripts/prep-vm.ps1
@@ -1,7 +1,7 @@
 # Install modules to update Windows
 Install-PackageProvider -Name "Nuget" -Force:$true -Confirm:$false
 Install-Module PSWindowsUpdate -Confirm:$false -Force:$true -SkipPublisherCheck:$true -Scope AllUsers
-Get-WUInstall –MicrosoftUpdate –AcceptAll -IgnoreReboot -Confirm:$false
+Get-WUInstall -MicrosoftUpdate -AcceptAll -IgnoreReboot -Confirm:$false
 
 # Generalize the VM
 $sysprepPath = $env:windir + '\System32\Sysprep\sysprep.exe'
