6d7eb4c4ca2fccf0451000326961cee2b7e5886c
Stanley Lam <lam.stanley@gmail.com>
2018-10-22 15:43:42 +0800

    Remove spaces between commands

--- vm-from-image/scripts/prep-vm.ps1
+++ vm-from-image/scripts/prep-vm.ps1
@@ -2,7 +2,6 @@
 Install-PackageProvider -Name "Nuget" -Force:$true -Confirm:$false
 Install-Module PSWindowsUpdate -Confirm:$false -Force:$true -SkipPublisherCheck:$true -Scope AllUsers
 Get-WUInstall -MicrosoftUpdate -AcceptAll -IgnoreReboot -Confirm:$false
-
 # Generalize the VM
 $sysprepPath = $env:windir + '\System32\Sysprep\sysprep.exe'
 Start-Process -FilePath $sysprepPath -ArgumentList ("/oobe /shutdown /generalize /quiet")
