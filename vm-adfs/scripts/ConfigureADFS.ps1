e06b77698c8bdc2a773e40af34cb4ef9975c0d43
Stanley Lam <lam.stanley@gmail.com>
2019-09-13 10:12:35 -0400

    Testing DSC

--- vm-adfs/scripts/ConfigureADFS.ps1
+++ vm-adfs/scripts/ConfigureADFS.ps1
@@ -79,6 +79,61 @@ configuration ConfigureADFS
             DependsOn = "[xCredSSP]CredSSPServer" 
         }
 
+        #**********************************************************
+        # Join AD forest
+        #**********************************************************
+        xWaitForADDomain DscForestWait
+        {
+            DomainName           = $DomainFQDN
+            RetryCount           = $RetryCount
+            RetryIntervalSec     = $RetryIntervalSec
+            DomainUserCredential = $DomainAdminCredsQualified
+            DependsOn            = "[xCredSSP]CredSSPClient"
+        }
+
+        Computer DomainJoin
+        {
+            Name       = $ComputerName
+            DomainName = $DomainFQDN
+            Credential = $DomainAdminCredsQualified
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+        xScript CreateWSManSPNsIfNeeded
+        {
+            SetScript =
+            {
+                # A few times, deployment failed because of this error:
+                # "The WinRM client cannot process the request. A computer policy does not allow the delegation of the user credentials to the target computer because the computer is not trusted."
+                # The root cause was that SPNs WSMAN/SP and WSMAN/sp.contoso.local were missing in computer account contoso\SP
+                # Those SPNs are created by WSMan when it (re)starts
+                # Restarting service causes an error, so creates SPNs manually instead
+                # Restart-Service winrm
+
+                # Create SPNs WSMAN/SP and WSMAN/sp.contoso.local
+                $domainFQDN = $using:DomainFQDN
+                $computerName = $using:ComputerName
+                Write-Verbose -Message "Adding SPNs 'WSMAN/$computerName' and 'WSMAN/$computerName.$domainFQDN' to computer '$computerName'"
+                setspn.exe -S "WSMAN/$computerName" "$computerName"
+                setspn.exe -S "WSMAN/$computerName.$domainFQDN" "$computerName"
+            }
+            GetScript = { return @{ "Result" = "false" } } # This block must return a hashtable. The hashtable must only contain one key Result and the value must be of type String.
+            TestScript = 
+            {
+                $computerName = $using:ComputerName
+                $samAccountName = "$computerName$"
+                if ((Get-ADComputer -Filter {(SamAccountName -eq $samAccountName)} -Property serviceprincipalname | Select-Object serviceprincipalname | Where-Object {$_.ServicePrincipalName -like "WSMAN/$computerName"}) -ne $null) {
+                    # SPN is present
+                    return $true
+                }
+                else {
+                    # SPN is missing and must be created
+                    return $false
+                }
+            }
+            DependsOn="[Computer]DomainJoin"
+        }
+
         
     }
 }
