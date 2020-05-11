b21c9140719d82d6e88d501a6be7acd9948a8381
Stanley Lam <lam.stanley@gmail.com>
2020-05-11 16:35:58 +0800

    Added script to install IIS + Node.js

---
+++ vm-iis-nodejs/scripts/iis-config.ps1
@@ -0,0 +1,56 @@
+Configuration ConfigureWebServer
+{
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    node localhost
+    {
+        LocalConfigurationManager 
+        {
+            ConfigurationMode  = "ApplyOnly"
+            ActionAfterReboot  = 'ContinueConfiguration'
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature InstallWebServer
+        {
+            Ensure = "Present"
+            Name = "Web-Server"
+        }
+
+        WindowsFeature InstallManagementTools
+        {
+            Ensure = "Present"
+            Name = "Web-Mgmt-Tools"
+        }
+
+        #**********************************************************
+        # Download .NET Framework
+        #**********************************************************
+        File DownloadFolder
+        {
+            DestinationPath = "C:\Downloads"
+            Type = "Directory"
+            Ensure = "Present"
+        }
+
+        #**********************************************************
+        # Install Node.js 8.9.4
+        #**********************************************************
+        xRemoteFile DownloadNodeJS
+        {
+            Uri = "https://nodejs.org/download/release/v8.9.4/node-v8.9.4-x64.msi"
+            DestinationPath = "C:\Downloads\node-v8.9.4-x64.msi"
+            MatchSource = $false
+            DependsOn = "[File]DownloadFolder"
+        }
+
+        xPackage InstallNodeJS 
+        {
+            Name = "Node.js"
+            ProductId = ""
+            Arguments = "/q /norestart /log C:\Downloads\dotnet-fx-install.log"
+            Path = "C:\Downloads\node-v8.9.4-x64.msi"
+            DependsOn = "[WindowsFeature]InstallWebServer", "[xRemoteFile]DownloadDotNetFramework"
+        }
+    }
+}
