36c107027ec17cc4ef1ad5e4e652f169b7a74ad6
Stanley Lam <lam.stanley@gmail.com>
2020-05-18 21:37:18 +0800

    Fixed script to install Node.js process manager

--- vm-iis-nodejs/scripts/iis-config.ps1
+++ vm-iis-nodejs/scripts/iis-config.ps1
@@ -1,14 +1,6 @@
 Configuration ConfigureWebServer
 {
-    param
-    (
-        [Parameter(Mandatory)]
-        [System.Management.Automation.PSCredential]$ScriptRunAsCreds
-    )
-
     Import-DscResource -ModuleName xPSDesiredStateConfiguration, PSDesiredStateConfiguration
-
-    [string] $PM2Home = "C:\etc\.pm2"
 
     node localhost
     {
@@ -60,81 +52,24 @@ Configuration ConfigureWebServer
             Path = "C:\Downloads\node-v8.9.4-x64.msi"
             DependsOn = "[WindowsFeature]InstallWebServer", "[xRemoteFile]DownloadNodeJS"
         }
-
-        #**********************************************************
-        # PM2 Home Directory
-        #**********************************************************
-        File PM2HomeDirectory
-        {
-            DestinationPath = $PM2Home
-            Type = "Directory"
-            Ensure = "Present"
-        }
-
-        xScript GrantPM2FullControlToLocalService
-        {
-            SetScript =
-            {
-                Write-Verbose -Message "Attempting to give LOCAL SERVICE FullControl acccess PM2 directory"
-                $pm2Home = $using:PM2Home
-                New-Item -ItemType Directory -Force -Path $pm2Home
-                $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("LOCAL SERVICE", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
-                $acl = Get-Acl -Path $pm2Home -ErrorAction Stop
-                $acl.SetAccessRule($rule)
-                Set-Acl -Path $pm2Home -AclObject $acl -ErrorAction Stop
-                Write-Host "Successfully set FullControl permissions on $pm2Home"
-            }
-            GetScript = 
-            {
-                # This block must return a hashtable. The hashtable must only contain one key Result and the value must be of type String.
-                return @{ "Result" = "false" } 
-            }
-            TestScript = 
-            {
-                # If it returns $false, the SetScript block will run. If it returns $true, the SetScript block will not run.
-                return $false
-            }
-            DependsOn="[File]PM2HomeDirectory"
-        }
-
         
         #**********************************************************
         # Download PM2 Windows Service Installer
         #**********************************************************
-        File DownloadFolder
+        xRemoteFile DownloadPM2Installer
         {
-            DestinationPath = "C:\Downloads"
-            Type = "Directory"
-            Ensure = "Present"
-        }
-
-        xScript DownloadPM2Installer
-        {
-            SetScript = 
-            {
-                $Url = "https://github.com/jessety/pm2-installer/archive/master.zip"
-                $Output = "C:\Downloads\pm2-installer.zip"
-                Invoke-WebRequest -Uri $Url -OutFile $Output
-            }
-            GetScript =  
-            {
-                # This block must return a hashtable. The hashtable must only contain one key Result and the value must be of type String.
-                return @{ "Result" = "false" }
-            }
-            TestScript = 
-            {
-                # If it returns $false, the SetScript block will run. If it returns $true, the SetScript block will not run.
-               return $false
-            }
+            Uri = "https://github.com/jessety/pm2-installer/archive/master.zip"
+            DestinationPath = "C:\Downloads\pm2-installer.zip"
+            MatchSource = $false
             DependsOn = "[File]DownloadFolder"
         }
 
         Archive ExtractPM2Installer
         {
-            Destination = "C:\Downloads\pm2-installer"
+            Destination = "C:\Downloads\"
             Path = "C:\Downloads\pm2-installer.zip"
             Force = $true
-            DependsOn = "[xScript]DownloadPM2Installer"
+            DependsOn = "[xRemoteFile]DownloadPM2Installer", "[xPackage]InstallNodeJS"
         }
 
         #**********************************************************
@@ -144,34 +79,13 @@ Configuration ConfigureWebServer
         {
             SetScript =
             {
-                Write-Verbose -Message "Installing pm2 dependencies..."
-                Start-Process -FilePath "C:\Program Files\nodejs\npm.cmd" -ArgumentList "install", "bufferutil", "-g" -Wait
-                Start-Process -FilePath "C:\Program Files\nodejs\npm.cmd" -ArgumentList "install", "utf-8-validate", "-g" -Wait
-                Write-Verbose -Message "Finished installing pm2 dependencies..."
-                
-                Write-Verbose -Message "Installing pm2..."
-                Start-Process -FilePath "C:\Program Files\nodejs\npm.cmd" -ArgumentList "install", "pm2", "-g" -Wait
-                Write-Verbose -Message "Finished installing pm2..."
+                $pm2Installer = "C:\Downloads\pm2-installer-master"
 
-                Write-Verbose -Message "Installing pm2-windows-service npm module..."
-                Start-Process -FilePath "C:\Program Files\nodejs\npm.cmd" -ArgumentList "install", "pm2-windows-service", "-g" -Wait
-                Write-Verbose -Message "Finished installing pm2-windows-service npm module..."
+                Write-Verbose -Message "Configuring pm2 installer..."
+                Start-Process -FilePath cmd.exe -ArgumentList "/c", "cd $pm2Installer && npm run configure" -Wait
 
-                # Write-Verbose -Message "Adding npm modules to path..."
-                # $addPath = $env:APPDATA + "\npm"
-                # $regexAddPath = [regex]::Escape($addPath)
-                # $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch "^$regexAddPath\\?"}
-                # $env:Path = ($arrPath + $addPath) -join ';'
-                # [Environment]::SetEnvironmentVariable("Path", $env:Path, "Machine")
-                # Write-Verbose -Message "Finished adding npm modules to path..."
-                
-                # $pm2Home = $using:PM2Home
-                # $env:PM2_HOME = $pm2Home
-                # [Environment]::SetEnvironmentVariable("PM2_HOME", $env:PM2_HOME, "Machine")
-
-                # Write-Verbose -Message "Installing pm2 as a service..."
-                # Start-Process -FilePath cmd.exe -ArgumentList "/c", "$env:APPDATA\npm\pm2-service-install" -Wait
-                # Write-Verbose -Message "Finished installing pm2 as a service..."
+                Write-Verbose -Message "Installing pm2 service..."
+                Start-Process -FilePath cmd.exe -ArgumentList "/c", "cd $pm2Installer && npm run setup" -Wait
             }
             GetScript = 
             {
@@ -183,8 +97,7 @@ Configuration ConfigureWebServer
                 # If it returns $false, the SetScript block will run. If it returns $true, the SetScript block will not run.
                 return $false
             }
-            DependsOn="[xScript]GrantPM2FullControlToLocalService"
-            PsDscRunAsCredential = $ScriptRunAsCreds
+            DependsOn="[Archive]ExtractPM2Installer"
         }
     }
 }
